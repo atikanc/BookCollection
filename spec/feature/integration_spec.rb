063187493f18599e4e4df6f0624b02137d2d0bf1
atikanc <atikanchokpapone@gmail.com>
2022-02-02 18:17:09 -0600

    Removed ignored files

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -22,4 +22,22 @@ RSpec.describe 'Creating a book with all attributes', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+
+  scenario 'invalid inputs (3)' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).not_to have_content('harry potter')
+  end
+
+  scenario 'invalid inputs (1)' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'bald potter'
+    fill_in 'Price', with: '123321'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).not_to have_content('harry potter')
+  end
 end
